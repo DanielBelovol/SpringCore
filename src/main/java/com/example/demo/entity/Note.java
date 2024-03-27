7bdfca854b050263f78a8bb995dfd193cb4f0c27
DanielBelovol <danielbelovol08@gmail.com>
2024-03-27 18:54:47 +0200

    Fix problem with lombok

--- src/main/java/com/example/demo/entity/Note.java
+++ src/main/java/com/example/demo/entity/Note.java
@@ -1,13 +1,10 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 @Entity
 @Data
--- src/main/java/com/example/demo/request/NoteCreateRequest.java
+++ src/main/java/com/example/demo/request/NoteCreateRequest.java
@@ -1,7 +1,7 @@
 package com.example.demo.request;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
+
 import lombok.Data;
 
 @Data
